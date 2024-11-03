aaa7b705fb1456babb2f03167fbcac6142581ba4
Hendrik Rauh <114620133+HendrikRauh@users.noreply.github.com>
2024-11-03 16:46:43 +0100

    removed filehead of old LIB

--- src/ESPDMX.h
+++ src/ESPDMX.h
@@ -1,15 +1,3 @@
-// - - - - -
-// ESPDMX - A Arduino library for sending and receiving DMX using the builtin serial hardware port.
-// ESPDMX.cpp: Library implementation file
-//
-// Copyright (C) 2015  Rick <ricardogg95@gmail.com>
-// This work is licensed under a GNU style license.
-//
-// Last change: Hendrik Rauh <https://github.com/Hendrik Rauh>
-//
-// Documentation and samples are available at https://github.com/Rickgg/ESP-Dmx
-// - - - - -
-
 #include <inttypes.h>
 
 #ifndef ESPDMX_h
