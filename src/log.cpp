f7522faf5c757b08dda8058656e5f8eed1a06562
HendrikRauh <114620133+HendrikRauh@users.noreply.github.com>
2025-05-27 21:46:07 +0200

    refactor writeLogEntry: include timestamp in log output for better traceability

--- src/log.cpp
+++ src/log.cpp
@@ -46,6 +46,6 @@ void writeLogEntry(const log_level level, const char *tag, const char *message, 
     char *buffer = new char[size + 1];
     va_start(args, message);
     vsnprintf(buffer, size + 1, message, args);
-    Serial.printf("[%s][%s]> %s\n", getLogLevelString(level), tag, buffer);
+    Serial.printf("[%s][%s](%d)> %s\n", getLogLevelString(level), tag, millis(), buffer);
     delete[] buffer;
 }
