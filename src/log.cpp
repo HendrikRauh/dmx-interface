d520fc8dfa2e0a47c2e9ef639d2249e7bee66a4a
HendrikRauh <114620133+HendrikRauh@users.noreply.github.com>
2025-05-27 18:24:43 +0200

    refactor writeLogEntry: dynamically allocate buffer for variable-length log messages

--- src/log.cpp
+++ src/log.cpp
@@ -13,11 +13,14 @@ void setupLogger()
 
 void writeLogEntry(const log_level level, const log_tag tag, const char *message, ...)
 {
-    char buffer[256];
     va_list args;
     va_start(args, message);
-    vsnprintf(buffer, sizeof(buffer), message, args);
+    int size = vsnprintf(nullptr, 0, message, args);
     va_end(args);
 
+    char *buffer = new char[size + 1];
+    va_start(args, message);
+    vsnprintf(buffer, size + 1, message, args);
     Serial.printf("[%d] [%d] \t %s\n", level, tag, buffer);
+    delete[] buffer;
 }
