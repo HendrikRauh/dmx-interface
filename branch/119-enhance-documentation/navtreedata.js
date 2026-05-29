45398de78ef6d2f5a98cfdc6235fefab3d9517cc
HendrikRauh <114620133+HendrikRauh@users.noreply.github.com>
2026-05-29 21:56:18 +0000

    Deploying to gh-pages from @ HendrikRauh/dmx-interface@d7d2b9804c7ec79a94d66742fd7cae85a4c30dd0 🚀

--- branch/119-enhance-documentation/navtreedata.js
+++ branch/119-enhance-documentation/navtreedata.js
@@ -24,8 +24,23 @@
 */
 var NAVTREE =
 [
-  [ "DMX-Interface", "index.html", [
-    [ "ChaosDMX", "index.html", "index" ],
+  [ "ChaosDMX", "index.html", [
+    [ "🛒 Parts", "index.html#autotoc_md-parts", null ],
+    [ "🔌 Wiring", "index.html#autotoc_md-wiring", null ],
+    [ "🚀 Installation", "index.html#autotoc_md-installation", null ],
+    [ "📦 Case", "index.html#autotoc_md-case", null ],
+    [ "💡 Status LED", "index.html#autotoc_md-status-led", null ],
+    [ "⚙️ Config", "index.html#️-config", [
+      [ "Default config", "index.html#default-config", null ]
+    ] ],
+    [ "🧑‍💻 Development", "index.html#‍-development", [
+      [ "Setup NIX", "index.html#setup-nix", null ],
+      [ "Setup direnv (optional)", "index.html#setup-direnv-optional", null ],
+      [ "Commands", "index.html#commands", null ],
+      [ "Pre-commit hooks", "index.html#pre-commit-hooks", null ],
+      [ "Documentation", "index.html#documentation", null ]
+    ] ],
+    [ "🤝 Contributing", "index.html#autotoc_md-contributing", null ],
     [ "Namespaces", "namespaces.html", [
       [ "Namespace List", "namespaces.html", "namespaces_dup" ],
       [ "Namespace Members", "namespacemembers.html", [
