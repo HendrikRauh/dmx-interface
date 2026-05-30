07b898285b19ada4a7d7f74e1701a0388b91abe5
HendrikRauh <114620133+HendrikRauh@users.noreply.github.com>
2026-05-30 20:24:45 +0000

    Deploying to gh-pages from @ HendrikRauh/dmx-interface@14e680f1ada8d1fa506d163cbae7fa212429e114 🚀

--- branch/idf/navtreeindex0.js
+++ branch/idf/navtreeindex0.js
@@ -45,6 +45,7 @@ var NAVTREEINDEX0 =
 "index.html#pre-commit-hooks":[6,3],
 "index.html#setup-direnv-optional":[6,1],
 "index.html#setup-nix":[6,0],
+"index.html#troubleshooting":[6,5],
 "index.html#via-nixosorg":[6,0,1],
 "index.html#via-terminatesystems-recommended":[6,0,0],
 "index.html#‍-development":[6],
--- branch/idf/search/pages_a.js
+++ branch/idf/search/pages_a.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['terminate_20systems_20recommended_0',['via terminate.systems (recommended)',['../index.html#via-terminatesystems-recommended',1,'']]]
+  ['terminate_20systems_20recommended_0',['via terminate.systems (recommended)',['../index.html#via-terminatesystems-recommended',1,'']]],
+  ['troubleshooting_1',['Troubleshooting',['../index.html#troubleshooting',1,'']]]
 ];
