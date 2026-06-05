d56ab9f7054bc2749682e22194ffeb7f9efcdeb9
Hendrik Rauh <114620133+HendrikRauh@users.noreply.github.com>
2026-06-05 17:23:19 +0200

    fix(version): make version 7 char like on github

--- components/system/gen_version.cmake
+++ components/system/gen_version.cmake
@@ -1,5 +1,5 @@
 execute_process(
-  COMMAND git rev-parse --short=8 HEAD
+  COMMAND git rev-parse --short=7 HEAD
   WORKING_DIRECTORY ${SOURCE_DIR}
   OUTPUT_VARIABLE GIT_HASH
   OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
