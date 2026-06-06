0f94771318a1ccf0bb4b27fda0a03f86ebfc55ae
Hendrik Rauh <114620133+HendrikRauh@users.noreply.github.com>
2026-06-06 17:37:36 +0200

    feat(storage-info): added info about diskspace

--- main/dmx-interface.c
+++ main/dmx-interface.c
@@ -11,6 +11,7 @@
 #include "led.h"
 #include "logger.h"
 #include "nvs_flash.h"
+#include "storage.h"
 #include "web_server.h"
 #include "wifi.h"
 
@@ -103,6 +104,8 @@ void app_main(void) {
 
   LOGI("Web server started successfully");
   LOGI("Open http://192.168.4.1 in your browser");
+
+  storage_print_info();
 
   vTaskDelay(pdMS_TO_TICKS(5000));
 
