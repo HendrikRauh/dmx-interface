f7ba825299e786a855e245f98c854bd34b2e8630
Hendrik Rauh <114620133+HendrikRauh@users.noreply.github.com>
2026-06-06 17:47:03 +0200

    feat(system-info): added info getter (#136)

--- main/dmx-interface.c
+++ main/dmx-interface.c
@@ -11,6 +11,7 @@
 #include "led.h"
 #include "logger.h"
 #include "nvs_flash.h"
+#include "system.h"
 #include "web_server.h"
 #include "wifi.h"
 
@@ -25,6 +26,8 @@ void app_main(void) {
 
   ESP_ERROR_CHECK(led_init());
   led_set_mode(LED_MODE_BOOT_BREATHING);
+
+  ESP_ERROR_CHECK(system_init());
 
   // Basic system init needed for the button callback to work safely
   esp_err_t err = nvs_flash_init();
@@ -108,6 +111,8 @@ void app_main(void) {
 
   led_set_brightness(config_get_led_brightness());
   led_set_mode(LED_MODE_NORMAL);
+
+  system_print_info();
 
   while (1) {
     vTaskDelay(pdMS_TO_TICKS(1000));
