4372292f2bed1f80f0e805568f4b7c723ad38f7f
RaffaelW <146560011+RaffaelW@users.noreply.github.com>
2026-06-10 21:17:57 +0000

    Deploying to gh-pages from @ HendrikRauh/dmx-interface@8eed4c8bf519054e9c6deb4a1918727acbbb6481 🚀

--- branch/idf/navtreedata.js
+++ branch/idf/navtreedata.js
@@ -49,7 +49,8 @@ var NAVTREE =
       [ "Namespace List", "namespaces.html", "namespaces_dup" ],
       [ "Namespace Members", "namespacemembers.html", [
         [ "All", "namespacemembers.html", null ],
-        [ "Functions", "namespacemembers_func.html", null ]
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ]
       ] ]
     ] ],
     [ "Data Structures", "annotated.html", [
@@ -78,7 +79,7 @@ var NAVTREE =
 var NAVTREEINDEX =
 [
 "_2home_2runner_2work_2dmx-interface_2dmx-interface_2components_2logger_2include_2logger_8h-example.html",
-"storage_8c.html#a71cbd7a3f317ed1014ad54c75976c040"
+"storage_8c.html#a41c0ea7e76221d162880e015b8f305d7"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronization';
--- branch/idf/search/all_2.js
+++ branch/idf/search/all_2.js
@@ -3,7 +3,7 @@ var searchData=
   ['btn_5faction_5fdouble_0',['btn_action_double',['../struct____attribute____.html#a1d8e2ca76026999dc489028230d80ac6',1,'__attribute__']]],
   ['btn_5faction_5fmulti_1',['btn_action_multi',['../struct____attribute____.html#a82c509daa8b09e7daceb4a867c0268e6',1,'__attribute__']]],
   ['btn_5faction_5fsingle_2',['btn_action_single',['../struct____attribute____.html#a173f1150f3c9a070998b5d79b8fc305a',1,'__attribute__']]],
-  ['build_3',['build',['../namespacetasks.html#a4a34f038ae2e8930e4a7f553047e01a7',1,'tasks']]],
+  ['build_3',['build',['../namespacetasks.html#a0308a0613f3f6fa6ca7d29c0de08de1f',1,'tasks']]],
   ['button_5factions_2ec_4',['button_actions.c',['../button__actions_8c.html',1,'']]],
   ['button_5factions_2eh_5',['button_actions.h',['../button__actions_8h.html',1,'']]],
   ['button_5fgpio_5fnum_6',['BUTTON_GPIO_NUM',['../button__actions_8c.html#a5dfac2fb10804bef66ef74d2fe13d3cf',1,'button_actions.c']]],
