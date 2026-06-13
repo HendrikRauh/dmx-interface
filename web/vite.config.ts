c326baea51ea4f18273d73a6b3ed9bf40c0321d1
RaffaelW <146560011+RaffaelW@users.noreply.github.com>
2026-06-13 23:53:27 +0200

    feat(web): embed favicon directly in the html

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,3 +1,6 @@
+import fs from "fs";
+import path from "path";
+
 import preact from "@preact/preset-vite";
 import { defineConfig } from "vite";
 import { mockDevServerPlugin } from "vite-plugin-mock-dev-server";
@@ -11,5 +14,36 @@ export default defineConfig({
     mockDevServerPlugin({
       prefix: ["/api/config"],
     }),
+    {
+      name: "inline-favicon",
+      transformIndexHtml(html) {
+        const generateFavicon = (file: string, colorscheme?: string) => {
+          const faviconPath = path.resolve(__dirname, file);
+          const base64 = fs.readFileSync(faviconPath).toString("base64");
+          const mimeType = "image/svg+xml";
+          const dataUri = `data:${mimeType};base64,${base64}`;
+
+          return {
+            tag: "link",
+            attrs: {
+              rel: "icon",
+              type: mimeType,
+              href: dataUri,
+              ...(colorscheme && { media: `(prefers-color-scheme: ${colorscheme})` }),
+            },
+            injectTo: "head" as const,
+          };
+        };
+
+        return {
+          html,
+          tags: [
+            generateFavicon("src/assets/favicon-mocha.svg"),
+            generateFavicon("src/assets/favicon-latte.svg", "light"),
+            generateFavicon("src/assets/favicon-mocha.svg", "dark"),
+          ],
+        };
+      },
+    },
   ],
 });
