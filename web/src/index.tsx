dd4e458aa2193f81ed314869144b1857266f4250
RaffaelW <146560011+RaffaelW@users.noreply.github.com>
2026-06-16 22:56:41 +0200

    feat(web): use correct logo based on theme

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,6 +1,7 @@
 import { render } from "preact";
 
-import favicon from "./assets/favicon-mocha.svg";
+import faviconLight from "./assets/favicon-latte.svg";
+import faviconDark from "./assets/favicon-mocha.svg";
 import { DmxForm } from "./components/dmx-form/DmxForm";
 
 import "./style.scss";
@@ -10,7 +11,10 @@ export function App() {
     <>
       <main>
         <header>
-          <img src={favicon} width="64" alt="ChaosDMX Logo" />
+          <picture>
+            <source srcset={faviconDark} media="(prefers-color-scheme: dark)" />
+            <img src={faviconLight} width="64" alt="ChaosDMX Logo" />
+          </picture>
           <h1>ChaosDMX</h1>
         </header>
         <DmxForm></DmxForm>
